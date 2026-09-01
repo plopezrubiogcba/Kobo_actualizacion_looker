ca89024ef74d3e8ac448d4eca8131d95b110edd4
facundocornetgcba <facundocornetgcba@gmail.com>
2026-09-01 16:50:14 +0000

    fix: usar OpenStreetMap como basemap definitivo (keyless, zoom hasta 19)

--- dashboard/src/modules/flash/FlashMapa.tsx
+++ dashboard/src/modules/flash/FlashMapa.tsx
@@ -152,8 +152,8 @@ export const FlashMapa = () => {
           className="bg-slate-900"
         >
           <TileLayer
-            url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
-            attribution='&copy; <a href="https://www.esri.com/">Esri</a>'
+            url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
           {geojson && (
             <GeoJSON
