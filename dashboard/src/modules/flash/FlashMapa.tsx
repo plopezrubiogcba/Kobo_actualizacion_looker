2be7fdcc08ff51c03779832bcf64ef7ec4469c37
facundocornetgcba <facundocornetgcba@gmail.com>
2026-09-01 16:45:18 +0000

    fix: reemplazar basemap CARTO por Esri Dark Gray (sin API key ni avisos)

--- dashboard/src/modules/flash/FlashMapa.tsx
+++ dashboard/src/modules/flash/FlashMapa.tsx
@@ -152,8 +152,8 @@ export const FlashMapa = () => {
           className="bg-slate-900"
         >
           <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-            attribution='&copy; <a href="https://carto.com/">CARTO</a>'
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+            attribution='&copy; <a href="https://www.esri.com/">Esri</a>'
           />
           {geojson && (
             <GeoJSON
